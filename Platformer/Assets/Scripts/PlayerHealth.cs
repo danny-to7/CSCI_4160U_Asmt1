34274a324d2424d89cf41735eef3f1cbfaf3c29c
danny-to7 <danny.to@uoit.net>
2019-02-08 00:02:08 -0500

    Initial commit
    -2D platformer level
    -basic sprite animation
    -resets on player death (fall off level)

---
+++ Platformer/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public bool playerDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -10)
+        {
+            playerDead = true;
+            StartCoroutine("Die");
+        }
+    }
+
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("Level");
+        yield return null;
+    }
+}
